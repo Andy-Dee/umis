4793d68809834960d0123187b8de0352f421f55d
Andy-Dee <andy.danilyuk@gmail.com>
2016-08-20 10:55:48 +0300

    no message

--- js/script.js
+++ js/script.js
@@ -49,4 +49,88 @@ $( function() {
     });
 });
 
+/* smooth scrolling */
+$(document).ready(function(){
+  $('a[href^="#"]').on('click', function(event) {
+      var target = $(this.getAttribute('href'));
+      if( target.length ) {
+          event.preventDefault();
+          $('html, body').stop().animate({
+              scrollTop: target.offset().top
+          }, 3000);
+      }
+  });
+});
+
+/* preload of hovers in cache */
+$(document).ready(function(){
+  (function ($) {
+    var cache = [];
+    $.preLoadImages = function () {
+        var args_len = arguments.length;
+        for (var i = args_len; i--; ) {
+            var cacheImage = document.createElement('img');
+            cacheImage.src = arguments[i];
+            cache.push(cacheImage);
+        }
+    }
+  })(jQuery)
+  jQuery.preLoadImages("./img/choose-hover.png", "./img/details-color-hover.png", "./img/details-white-hover.png", 
+    "./img/fancy-close-hover.png", "./img/free-call-hover.png", "./img/send-apply-hover.png", "./img/slide-prev-hover.png",
+    "./img/slide-next-hover.png");
+});
+
+/* hover of  */
+
+$( function() {
+    $('.free-call').hover(
+    function(){
+        $('.free-call-link').css('color','white');
+    },
+    function(){
+        $('.free-call-link').css('color','#454545');
+    });
+});
+
+
+/* Input file castomisation */
+$(function(){
+    var wrapper = $( ".file_upload" ),
+        inp = wrapper.find( "input" ),
+        btn = wrapper.find( ".button" ),
+        lbl = wrapper.find( "mark" );
+
+    // Crutches for the :focus style:
+    inp.focus(function(){
+        wrapper.addClass( "focus" );
+    }).blur(function(){
+        wrapper.removeClass( "focus" );
+    });
+
+    var file_api = ( window.File && window.FileReader && window.FileList && window.Blob ) ? true : false;
+
+    inp.change(function(){
+        var file_name;
+        if( file_api && inp[ 0 ].files[ 0 ] )
+            file_name = inp[ 0 ].files[ 0 ].name;
+        else
+            file_name = inp.val().replace( "C:\\fakepath\\", '' );
+
+        if( ! file_name.length )
+            return;
+
+        if( lbl.is( ":visible" ) ){
+            lbl.text( file_name );
+            btn.text( "Выбрать" );
+        }else
+            btn.text( file_name );
+    }).change();
+
+});
+$( window ).resize(function(){
+    $( ".file_upload input" ).triggerHandler( "change" );
+});
+
+
+
 
